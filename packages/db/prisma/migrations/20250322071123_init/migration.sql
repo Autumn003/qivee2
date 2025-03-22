d7f5f38426e1ba1ff3fa706a2fbb9334ea6fa89d
Hemant <davidbroddy1@gmail.com>
2025-03-22 13:13:58 +0530

    updated user schema

--- packages/db/prisma/migrations/20250322071123_init/migration.sql
+++ packages/db/prisma/migrations/20250322071123_init/migration.sql
@@ -2,10 +2,15 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
+    "number" TEXT,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_number_key" ON "User"("number");
