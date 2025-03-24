980b8cb229c31f20545d719fb0fef9531f865ecd
Kunal Sharma <davidbroddy1@gmail.com>
2025-03-24 16:08:42 +0530

    `feat: updated product actions, added update and delete action`

--- apps/web/actions/product.action.ts
+++ apps/web/actions/product.action.ts
@@ -3,6 +3,25 @@
 import db from "@repo/db/client";
 import { productSchema } from "schemas/product-schema";
 
+export async function getAllProducts() {
+  try {
+    const products = await db.product.findMany({
+      orderBy: { createdAt: "desc" }, // Sorting by latest products first
+    });
+
+    // Convert Decimal price to number
+    const serializedProducts = products.map((product) => ({
+      ...product,
+      price: Number(product.price), // Convert Decimal to number
+    }));
+
+    return { success: true, products: serializedProducts };
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return { error: "Failed to fetch products" };
+  }
+}
+
 export async function createProduct(formData: FormData) {
   const rawData = Object.fromEntries(formData);
 
@@ -38,21 +57,53 @@ export async function createProduct(formData: FormData) {
   return { success: true, product, message: "Product created successfully" };
 }
 
-export async function getAllProducts() {
-  try {
-    const products = await db.product.findMany({
-      orderBy: { createdAt: "desc" }, // Sorting by latest products first
-    });
+export async function updateProduct(id: string, formData: FormData) {
+  const rawData = Object.fromEntries(formData);
 
-    // Convert Decimal price to number
-    const serializedProducts = products.map((product) => ({
-      ...product,
-      price: Number(product.price), // Convert Decimal to number
-    }));
+  const images = formData
+    .getAll("images[]")
+    .map((img) => (typeof img === "string" ? img : ""))
+    .filter(Boolean) as string[];
 
-    return { success: true, products: serializedProducts };
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    return { error: "Failed to fetch products" };
+  const parsedData = productSchema.safeParse({
+    ...rawData,
+    price: Number(rawData.price),
+    stock: Number(rawData.stock),
+    images,
+  });
+
+  if (!parsedData.success) {
+    return { error: parsedData.error.flatten() };
   }
+
+  const { name, description, price, stock, category } = parsedData.data;
+
+  const updatedProduct = await db.product.update({
+    where: { id },
+    data: {
+      name,
+      description,
+      price,
+      images,
+      stock,
+      category,
+    },
+  });
+
+  return {
+    success: true,
+    updatedProduct,
+    message: "Product updated successfully",
+  };
+}
+
+export async function deleteProduct(id: string) {
+  await db.product.delete({
+    where: { id },
+  });
+
+  return {
+    success: true,
+    message: "Product deleted successfully",
+  };
 }
