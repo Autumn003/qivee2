c9c7d31a4c65eb1e3df0a1f0c90b39772cfdf0d7
Kunal Sharma <davidbroddy1@gmail.com>
2025-05-07 17:37:45 +0530

    `Refactor middleware.ts to redirect unauthorized users and remove unused import`

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 import { UserRole } from "@prisma/client";
 
@@ -9,20 +7,24 @@ export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
 
   if (
-    token &&
-    (url.pathname.startsWith("/api/admin") ||
-      url.pathname.startsWith("/signin") ||
-      url.pathname.startsWith("/signup") ||
-      url.pathname.startsWith("/forget-password") ||
-      url.pathname.startsWith("/reset-password"))
+    url.pathname.startsWith("/admin") ||
+    url.pathname.startsWith("/api/admin")
   ) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+    if (!token) {
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
+
+    if (token.role !== UserRole.ADMIN) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
   }
 
   if (
     token &&
-    token.role === UserRole.USER &&
-    (url.pathname.startsWith("/admin") || url.pathname.startsWith("/api/admin"))
+    (url.pathname.startsWith("/signin") ||
+      url.pathname.startsWith("/signup") ||
+      url.pathname.startsWith("/forget-password") ||
+      url.pathname.startsWith("/reset-password"))
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
@@ -34,7 +36,6 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     "/signin",
@@ -43,6 +44,7 @@ export const config = {
     "/reset-password/:pathname*",
     "/dashboard/:path*",
     "/admin/:pathname*",
+    "/admin",
     "/api/admin/:pathname*",
   ],
 };
