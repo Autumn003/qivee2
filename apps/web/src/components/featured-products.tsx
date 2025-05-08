1432fdb31c79823d9a459dd1a7758d5ea768c282
Kunal Sharma <davidbroddy1@gmail.com>
2025-05-08 10:22:48 +0530

    `Added key prop to Link component in FeaturedProducts component`

--- apps/web/src/components/featured-products.tsx
+++ apps/web/src/components/featured-products.tsx
@@ -62,7 +62,7 @@ export default function FeaturedProducts({ count, variant }: Props) {
       </div>
       <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {featuredProducts.map((product) => (
-          <Link href={`/products/${product.id}`}>
+          <Link href={`/products/${product.id}`} key={product.id}>
             <FeaturedCard
               title={product.name}
               imageURL={product.images[0] || ""}
